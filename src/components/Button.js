10f517bf10c9499b23dbb6c1ec771372c50fd58a
Jatin957 <jatinc957@gmail.com>
2025-04-26 00:51:29 +0530

    node modules

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,10 @@
-
 import React from "react";
 
 const Button = ({ children, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="px-4 py-2 bg-blue-600 text-white rounded-xl hover:bg-blue-700 transition"
+      className="px-4 py-2 bg-blue-600 text-white rounded-xl hover:bg-blue-700 hover:scale-105 transition-all duration-300 shadow-md"
     >
       {children}
     </button>
